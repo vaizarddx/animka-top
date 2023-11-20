20801f1b893110081d24566054b86e80655376c9
vaizarddx <151498726+vaizarddx@users.noreply.github.com>
2023-11-20 15:23:15 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+let menu =document.querySelector('#menu-bars');
+let navbar =  document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle ('fa-time');
+    navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 70,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".anime-slider", {
+    slidesPerView: 4,
+    spaceBetween: 70,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+
+
+  var swiper = new Swiper(".action-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+
+  
+  var swiper = new Swiper(".advise-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
